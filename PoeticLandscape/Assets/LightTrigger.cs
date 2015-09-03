8fe1684adbf2ecbfed3c49c7201481971370c0c6
Christina Goefron <cagoefron@gmail.com>
2015-09-03 17:35:58 -0400

    Lab 2 Triggers

---
+++ PoeticLandscape/Assets/LightTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	// When the player walks into a trigger on this gameobject
+	void OnTriggerEnter (Collider other) {
+            Debug.Log ("The Trigger is working!");
+	// The light component on this game object, turn it off
+		    gameObject.GetComponent<Light> ().enabled = false;
+	}
+	void OnTriggerExit (Collider other) {
+			Debug.Log ("Some shit");
+			gameObject.GetComponent<Light> ().enabled = true; 
+		
+}
+	void OnTriggerEnter() {
+		gameObject.GetComponent<MeshRenderer>().material.color = Color.red;
+	}
+}
